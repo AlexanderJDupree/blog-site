acd50827ea2e8f77facf53486ab67d4a9492106e
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-04-19 12:04:31 -0700

    Mounted static/ for stylesheets and assets

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,23 @@
-//! *blog-site* crate for my personal portfolio and blogging website. 
-//! 
+//! *blog-site* crate for my personal portfolio and blogging website.
+//!
 //! adupree.dev
-//! 
+//!
 //! https://github.com/AlexanderJDupree/blog-site
-//! 
+//!
 
 #![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 extern crate rocket_contrib;
 
+use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
 
 mod routes;
 
 pub fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/", routes![
-            routes::home::index,
-        ])
+        .mount("/", routes![routes::home::index,])
+        .mount("/static", StaticFiles::from("./static/"))
         .attach(Template::fairing())
 }

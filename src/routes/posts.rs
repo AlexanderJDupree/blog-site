0aafd4a3e9a53cab6474e28f90b51392acd1f137
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-04-28 23:06:20 -0700

    Add post preview to endpoint

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -2,14 +2,24 @@
 use glob::glob;
 use rocket_contrib::json;
 use rocket_contrib::json::JsonValue;
+use serde::Serialize;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error};
+use std::path::PathBuf;
+
+#[derive(Serialize)]
+struct PostPreview {
+    title: String,
+    preview: String,
+}
 
 /// Get the content of a specific post
-#[get("/posts/<slug>")]
-pub fn get_post(slug: String) -> JsonValue {
+#[get("/posts/<title>")]
+pub fn get_post(title: String) -> JsonValue {
     json!({
         "id": 42,
-        "article": "lorem ipsum",
-        "slug": slug
+        "body": "lorem ipsum",
+        "title": title
     })
 }
 
@@ -20,16 +30,30 @@ pub fn get_posts(limit: Option<usize>, offset: Option<usize>) -> JsonValue {
     let offset = offset.unwrap_or(0);
 
     // TODO paramterize posts directory
-    let posts: Vec<std::path::PathBuf> = glob("posts/*.md")
+    let posts: Vec<PostPreview> = glob("posts/*.md")
         .unwrap()
         .skip(offset)
         .take(limit)
         .filter_map(Result::ok)
+        .map(fill_post_preview)
+        .filter_map(Result::ok)
         .collect();
 
     json!({
-        "limit": limit,
-        "offset": offset,
-        "posts": posts
+        "posts": posts,
     })
+}
+
+fn fill_post_preview(post: PathBuf) -> Result<PostPreview, Error> {
+    let title = post.file_name().unwrap().to_str().unwrap().to_owned();
+    let file = File::open(post)?;
+
+    // TODO: Read front matter into struct as well and then read first line as preview
+    let preview: String = BufReader::new(file)
+        .lines()
+        .take(1)
+        .filter_map(Result::ok)
+        .fold(String::new(), |acc, line| acc + line.as_str());
+
+    Ok(PostPreview { title, preview })
 }

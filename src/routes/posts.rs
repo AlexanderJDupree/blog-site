41e9c1fd137ba94f55d97e0f6363db08036d88ba
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-05-03 00:08:18 -0700

    Added endpoint for retrieving specific post

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 use serde_yaml;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 lazy_static! {
     static ref POSTS_DIR: String =
@@ -48,14 +48,25 @@ struct PostPreview {
     preview: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Post {
+    frontmatter: Frontmatter,
+    body: String,
+}
+
 /// Get the content of a specific post
 #[get("/posts/<title>")]
 pub fn get_post(title: String) -> JsonValue {
-    json!({
-        "id": 42,
-        "body": "lorem ipsum",
-        "title": title
-    })
+    let post_glob = format!("{}/*_{}.md", *POSTS_DIR, title);
+
+    let post: Vec<Post> = glob(&post_glob)
+        .unwrap()
+        .filter_map(Result::ok)
+        .map(read_post)
+        .filter_map(Result::ok)
+        .collect();
+
+    json!(post[0])
 }
 
 /// Get a paginated lists of post titles and descriptions
@@ -80,6 +91,38 @@ pub fn get_posts(limit: Option<usize>, offset: Option<usize>) -> JsonValue {
     })
 }
 
+fn read_post(path: PathBuf) -> Result<Post, Error> {
+    let frontmatter = read_frontmatter(&path)?;
+    // Read post body
+    let file = File::open(&path)?;
+    let reader = BufReader::new(file);
+
+    let body: String = reader
+        .lines()
+        .skip_while(|s| s.as_ref().unwrap_or(&"".to_string()) == "---")
+        .skip_while(|s| s.as_ref().unwrap_or(&"".to_string()) != "---")
+        .skip(1)
+        .skip_while(|s| s.as_ref().unwrap_or(&"".to_string()).trim().is_empty())
+        .filter_map(Result::ok)
+        .fold(String::new(), |acc, line| [acc, line].join("\n"));
+
+    Ok(Post { frontmatter, body })
+}
+
+fn read_frontmatter(path: &Path) -> Result<Frontmatter, Error> {
+    let file = File::open(&path)?;
+    let reader = BufReader::new(file);
+
+    let frontmatter: String = reader
+        .lines()
+        .skip_while(|s| s.as_ref().unwrap_or(&"".to_string()) == "---")
+        .take_while(|s| s.as_ref().unwrap_or(&"".to_string()) != "---")
+        .filter_map(Result::ok)
+        .fold(String::new(), |acc, line| [acc, line].join("\n"));
+
+    Ok(serde_yaml::from_str(&frontmatter).unwrap_or_default())
+}
+
 fn fill_post_preview(post: PathBuf) -> Result<PostPreview, Error> {
     // Parse filename into date, post title
     let components: Vec<&str> = post
@@ -97,21 +140,9 @@ fn fill_post_preview(post: PathBuf) -> Result<PostPreview, Error> {
     // Convert title to posts link
     let link = uri!(get_post: title = title.strip_suffix(".md").unwrap_or("invalid")).to_string();
 
-    // Read YAML Front Matter
-    let file = File::open(&post)?;
-    let reader = BufReader::new(file);
-
-    let frontmatter: String = reader
-        .lines()
-        .skip_while(|s| s.as_ref().unwrap_or(&"".to_string()) == "---")
-        .take_while(|s| s.as_ref().unwrap_or(&"".to_string()) != "---")
-        .filter_map(Result::ok)
-        .fold(String::new(), |acc, line| [acc, line].join("\n"));
-
-    let frontmatter: Frontmatter = serde_yaml::from_str(&frontmatter).unwrap_or_default();
+    let frontmatter = read_frontmatter(&post)?;
 
     // Read Post preview
-    // TODO: this is horrible, there must be a way to reuse the buf reader or something
     let file = File::open(&post)?;
     let reader = BufReader::new(file);
 

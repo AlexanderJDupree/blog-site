30ecfa5f78b116937868a06f8140a4a99329d6e6
Alexander Dupree <alexanderjdupree@gmail.com>
2021-05-10 23:24:08 -0700

    Added cors fairing

--- client/src/components/Blog.tsx
+++ client/src/components/Blog.tsx
@@ -1,17 +1,31 @@
 import { useState } from 'react';
-import { Card, CardDeck, Container, Pagination } from 'react-bootstrap';
+import {
+  Card,
+  CardDeck,
+  Container,
+  Pagination,
+  Spinner,
+} from 'react-bootstrap';
+import useFetch, { FetchStatus } from '../hooks/useFetch';
 
 const Fade = require('react-reveal/Fade');
 
-interface PostProps {
-  image: string;
+interface Frontmatter {
   title: string;
-  body: string;
   tags: string[];
+  categories: string[];
+  image: string;
 }
 
-const Post = ({ image, title, body, tags }: PostProps) => {
-  const links = tags.map((tag, i) => {
+interface PostPreview {
+  date: string;
+  link: string;
+  frontmatter: Frontmatter;
+  preview: string;
+}
+
+const Post = ({ date, link, frontmatter, preview }: PostPreview) => {
+  const links = frontmatter.tags.map((tag, i) => {
     return (
       <span className='tag' key={i}>
         #{tag}
@@ -22,9 +36,9 @@ const Post = ({ image, title, body, tags }: PostProps) => {
   return (
     <Card className='post shadow'>
       <Card.Body className='body'>
-        <Card.Title className='title'>{title}</Card.Title>
+        <Card.Title className='title'>{frontmatter.title}</Card.Title>
         <hr />
-        <Card.Text className='text'>{body.slice(0, 125)}...</Card.Text>
+        <Card.Text className='text'>{preview.slice(0, 125)}...</Card.Text>
         {links}
       </Card.Body>
     </Card>
@@ -32,56 +46,14 @@ const Post = ({ image, title, body, tags }: PostProps) => {
 };
 
 const Blog = () => {
-  const posts = [
-    {
-      image: 'http://placekitten.com/160/100',
-      title: 'Post Title',
-      body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam tempore officia quaerat, molestias corrupti aspernatur repudiandae nostrum modi, explicabo nam culpa dolorum harum delectus similique reiciendis eligendi voluptate reprehenderit? Velit?',
-      tags: ['react', 'typescript', 'rust'],
-    },
-
-    {
-      image: 'http://placekitten.com/160/100',
-      title: 'Post Title',
-      body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam tempore officia quaerat, molestias corrupti aspernatur repudiandae nostrum modi, explicabo nam culpa dolorum harum delectus similique reiciendis eligendi voluptate reprehenderit? Velit?',
-      tags: ['react', 'typescript', 'rust'],
-    },
-
-    {
-      image: 'http://placekitten.com/160/100',
-      title: 'Post Title',
-      body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam tempore officia quaerat, molestias corrupti aspernatur repudiandae nostrum modi, explicabo nam culpa dolorum harum delectus similique reiciendis eligendi voluptate reprehenderit? Velit?',
-      tags: ['react', 'typescript', 'rust'],
-    },
-
-    {
-      image: 'http://placekitten.com/160/100',
-      title: 'Post Title',
-      body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam tempore officia quaerat, molestias corrupti aspernatur repudiandae nostrum modi, explicabo nam culpa dolorum harum delectus similique reiciendis eligendi voluptate reprehenderit? Velit?',
-      tags: ['react', 'typescript', 'rust'],
-    },
-
-    {
-      image: 'http://placekitten.com/160/100',
-      title: 'Post Title',
-      body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam tempore officia quaerat, molestias corrupti aspernatur repudiandae nostrum modi, explicabo nam culpa dolorum harum delectus similique reiciendis eligendi voluptate reprehenderit? Velit?',
-      tags: ['react', 'typescript', 'rust'],
-    },
-  ].map((post, i) => {
-    return (
-      <Post
-        key={i}
-        image={post.image}
-        title={post.title}
-        body={post.body}
-        tags={post.tags}
-      />
-    );
-  });
-  const [page, setPage] = useState(0);
-
   const postsPerPage = 3;
-  const pages = Math.ceil(posts.length / postsPerPage);
+  const [page, setPage] = useState(0);
+  const res = useFetch<{ posts: PostPreview[] }>(
+    `http://localhost:8000/api/v1/posts?limit=1000`,
+    { posts: [] }
+  );
+
+  const pages = Math.ceil(res.data.posts.length / postsPerPage);
   const items = [...Array(pages)].map((_x, i) => {
     return (
       <Pagination.Item key={i} active={page === i} onClick={(e) => setPage(i)}>
@@ -90,18 +62,51 @@ const Blog = () => {
     );
   });
 
-  return (
-    <Container className='blog' id='blog'>
-      <Fade right>
-        <CardDeck>
-          {posts.slice(page * postsPerPage, page * postsPerPage + postsPerPage)}
-        </CardDeck>
-        <Pagination className='mt-3 mx-auto justify-content-center'>
-          {items}
-        </Pagination>
-      </Fade>
-    </Container>
-  );
+  switch (res.status) {
+    case FetchStatus.Loading:
+      return (
+        <Container className='blog justify-content-center' id='blog'>
+          <Spinner
+            animation='border'
+            role='status'
+            className='theme-secondary-light'
+          >
+            <span className='sr-only'>Loading...</span>
+          </Spinner>
+        </Container>
+      );
+    case FetchStatus.Error:
+      return (
+        <Container className='blog justify-content-center' id='blog'>
+          <div className='mx-auto mt-4'>Something went wrong... 🤷‍♂️</div>
+        </Container>
+      );
+
+    case FetchStatus.Loaded:
+      return (
+        <Container className='blog' id='blog'>
+          <Fade right>
+            <CardDeck>
+              {res.data.posts
+                .slice(page * postsPerPage, page * postsPerPage + postsPerPage)
+                .map((post) => {
+                  return (
+                    <Post
+                      date={post.date}
+                      link={post.link}
+                      frontmatter={post.frontmatter}
+                      preview={post.preview}
+                    />
+                  );
+                })}
+            </CardDeck>
+            <Pagination className='mt-3 mx-auto justify-content-center'>
+              {items}
+            </Pagination>
+          </Fade>
+        </Container>
+      );
+  }
 };
 
 export default Blog;
